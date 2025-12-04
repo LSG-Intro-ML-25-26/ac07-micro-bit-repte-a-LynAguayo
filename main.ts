5a63c3040228f71c7cfc89cc26dd8f8cf9bfc8e4
Lyn Aguayo <lyn.aguayoc@gmail.com>
2025-12-04 18:25:21 +0100

    Fix code

--- main.ts
+++ main.ts
@@ -3,8 +3,6 @@ input.onGesture(Gesture.Shake, function () {
     vTurno = randint(1, 3)
     if (vTurno == 1) {
         basic.showIcon(IconNames.SmallSquare)
-    } else if (false) {
-    	
     } else {
     	
     }
