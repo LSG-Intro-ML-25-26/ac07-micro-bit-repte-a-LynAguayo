01ec83b197e47dbc5d377fd2e99bdc55881bb766
Lyn Aguayo <lyn.aguayoc@gmail.com>
2025-12-04 18:13:31 +0100

    Define action when vTurno = 1

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 let vTurno = 0
 input.onGesture(Gesture.Shake, function () {
-    vTurno = randint(0, 3)
-    if (vTurno) {
+    vTurno = randint(1, 3)
+    if (vTurno == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (false) {
     	
     } else {
     	
