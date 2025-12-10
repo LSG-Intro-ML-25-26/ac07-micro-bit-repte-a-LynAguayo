d5f8bb0532fb98bf5c01c1d7a15dff5b928dffe9
Lyn Aguayo <lyn.aguayoc@gmail.com>
2025-12-10 20:56:35 +0100

    Init game when button shake is pressed

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-hand = 0
+game_active = False
+hand_player = 0
+hand_cpu = 0
 
 def on_gesture_shake():
-    global hand
-    hand = randint(1, 3)
-    if hand == 1:
-        basic.show_icon(IconNames.SMALL_SQUARE)
-    elif hand == 2:
-        basic.show_icon(IconNames.SQUARE)
-    else:
-        basic.show_icon(IconNames.SCISSORS)
+    global game_active, hand_player, hand_cpu
+    if not (game_active):
+        game_active = True
+        hand_player = 0
+        hand_cpu = 0
+        basic.show_string("START")
+        basic.show_icon(IconNames.HAPPY)
+        basic.pause(500)
+        basic.clear_screen()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
-let hand = 0
-input.onGesture(Gesture.Shake, function () {
-    hand = randint(1, 3)
-    if (hand == 1) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else if (hand == 2) {
-        basic.showIcon(IconNames.Square)
-    } else {
-        basic.showIcon(IconNames.Scissors)
+let game_active = false
+let hand_player = 0
+let hand_cpu = 0
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    if (!game_active) {
+        game_active = true
+        hand_player = 0
+        hand_cpu = 0
+        basic.showString("START")
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+        basic.clearScreen()
     }
+    
 })
